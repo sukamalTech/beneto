58debe5cdff17c3e39acd07325cfcc0de6d23b65
sukamalTech <147377668+sukamalTech@users.noreply.github.com>
2024-01-22 12:28:15 +0530

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,28 +5,28 @@ export default function Home() {
   return (
     <main className='w-full max-w-8xl h-full min-h-screen flex flex-col p-2 lg:p-10 bg-[#262626] lg:mx-auto'>
       <nav className='h16 w-full flex items-center justify-between'>
-        <div className='h-16 w-16 rounded-full bg-zinc-900 overflow-hidden flex items-center justify-center'>
+        <div className='h-16 w-16 rounded-full bg-zinc-900 overflow-hidden flex items-center justify-center hover:scale-95 transition duration-300'>
           <img src="/beneto/Mark.svg" alt="Menu image" className='h-10 w-10 object-contain' />
 
         </div>
 
         <div className="flex-1 h-16 px-2 lg:px-[66px] py-5 bg-zinc-900 rounded-[48px] border-2 border-neutral-800 justify-center items-center gap-[100px] lg:gap-[330px] inline-flex">
           <div className="text-center text-red-500 text-xl font-bold font-['Inter'] uppercase leading-normal">About</div>
-          <div className="hidden lg:flex text-center text-white text-xl font-normal font-['Inter'] uppercase leading-normal">Portfolio</div>
-          <div className="hidden lg:flex text-center text-white text-xl font-normal font-['Inter'] uppercase leading-normal">Contact</div>
+          <div className="hidden lg:flex text-center text-white text-xl font-normal font-['Inter'] uppercase hover:scale-5 hover:text-red-500 transition-all duration-300">Portfolio</div>
+          <div className="hidden lg:flex text-center text-white text-xl font-normal font-['Inter'] uppercase hover:scale-5 hover:text-red-500 transition-all duration-300 ">Contact</div>
           <div className='flex lg:hidden'><AlignJustify /></div>
         </div>
       </nav>
       <section className='w-full max-w-8xl border border-gray-700 rounded-xl mt-3 lg:h-[400px] h-auto lg:py-[60px] lg:px-[40px]'>
         <div className='flex flex-col lg:flex-row w-full items-center lg:justify-between mb-[70px] gap-5 p-5'>
           <h3 className='text-gray-300'>UX/UI DESIGNER</h3>
-          <div className='text-sm text-gray-200 flex items-center gap-2 bg-zinc-900 rounded-full py-1 px-4'>
-            <div className='h-2 w-2 rounded-full bg-red-500'></div>
+          <div className='text-sm text-gray-200 flex items-center gap-2 bg-zinc-900 rounded-full py-1 px-4 uppercase'>
+            <div className='h-2 w-2 rounded-full bg-red-500 '></div>
             Avaliable for job</div>
         </div>
         <div className='w-full lg:h-[190px] h-auto flex flex-col items-center lg:flex-row lg:justify-between gap-5 '>
           <div className=''>
-            <div className="w-72 text-white text-4xl font-semibold font-['Inter']">I’m Zainab Nisa</div>
+            <div className="w-72 lg:w-96 text-white text-4xl font-semibold font-['Inter']">I’m Zasmin Winslate</div>
             <div className="w-72 text-stone-300 text-xl font-normal font-['Inter'] leading-[30px]">I design digital products with <br />great experience.</div>
             <div className='w-72 flex items-baseline justify-between pt-5'>
               <div className="w-[115px] h-[42px] relative flex gap-[1px]">
@@ -67,7 +67,7 @@ export default function Home() {
           </div>
           <div className='mt-3 w-full h-auto border border-gray-700 rounded-xl pt-[50px] lg:px-[54px] flex flex-col gap-4'>
             <CardHeader heading="AI Map redesign" subheading="UX Case study" />
-            <div className='px-30 mt-30 h-auto w-full bg-white/10 overflow-hidden rounded-t-3xl'>
+            <div className='px-30 mt-30 h-auto w-full  overflow-hidden rounded-t-3xl'>
               <img src="/beneto/iPhone Space gray.png" alt="" className='h-full w-full object-contain' />
             </div>
           </div>
@@ -89,7 +89,7 @@ export default function Home() {
             <div className='border-t-2 border-red-600 rounded-tr-xl border-r-2 relative hover:border-red-500 '>
               <div className="relative group w-full h-auto border text-center  border-gray-700  rounded-xl py-[60px] lg:px-[40px]
                             text-stone-300 text-2xl font-medium font-['Inter'] overflow-hidden  ">
-                Product Design
+                Mobile App Design
                 <div className="absolute  top-0 right-0 h-[60%] w-[20%] bg-gradient-to-tr from-transparent rounded-full to-red-500  blur-2xl group-hover:blur-xl m-auto "></div>
               </div>
               <div className='absolute -top-0.5 left-0 h-[2px] w-full bg-gradient-to-l from-transparent via-[#262626] to-[#262626] from-[10%] via-[30%]' />
@@ -98,7 +98,7 @@ export default function Home() {
             <div className='border-b-2 border-red-600 rounded-bl-xl border-l-2 relative hover:border-red-500 '>
               <div className="relative group w-full h-auto border text-center  border-gray-700  rounded-xl py-[60px] lg:px-[40px]
                             text-stone-300 text-2xl font-medium font-['Inter'] overflow-hidden  ">
-                Product Design
+                Website Design
                 <div className="absolute  bottom-0 left-0 h-[60%] w-[20%] bg-gradient-to-bl from-transparent rounded-full to-red-500  blur-2xl group-hover:blur-xl m-auto   "></div>
               </div>
               <div className='absolute -bottom-0.5 right-0 h-[2px] w-full bg-gradient-to-r from-transparent via-[#262626] to-[#262626] from-[10%] via-[30%]' />
@@ -108,12 +108,15 @@ export default function Home() {
           </div>
           <div className='w-full h-[520px] lg:px-[54px] py-[40px] border border-gray-700 rounded-xl'>
             <CardHeader heading="AI Music Product" subheading="UX Case study" />
-            <div className='px-30 mt-30 h-auto w-full bg-white/10 overflow-hidden rounded-t-3xl'>
-              <img src="/beneto/iPhone.png" alt="" className='h-full w-full object-contain' />
+            <div className='px-5 lg:px-[30px] mt-[30px] h-auto w-full  '>
+              <img src="/beneto/project1.png" alt="" className='h-full w-full object-contain' />
             </div>
           </div>
           <div className='w-full h-[506px] lg:px-[54px] py-[40px] border border-gray-700 rounded-xl'>
             <CardHeader heading="AI Map Design" subheading="UX Case study" />
+            <div className='px-5 lg:px-[30px] mt-[30px] h-auto w-full  '>
+              <img src="/beneto/project2.png" alt="" className='h-full w-full object-contain' />
+            </div>
           </div>
 
         </div>
@@ -153,7 +156,8 @@ function CardHeader({ heading, subheading }: any) {
         <div className="text-white text-2xl font-semibold font-['Inter']">{heading}</div>
         <div className="text-stone-300 text-xl font-normal font-['Inter']">{subheading}</div>
       </div>
-      <div className="w-10 h-10 relative  bg-red-600 rounded-full shadow  border border-black text-white flex items-center justify-center" >
+      <div className="w-10 h-10 relative  bg-red-600 rounded-full shadow  border border-black text-white flex items-center justify-center
+      hover:scale-95 transition duration-300" >
         <ArrowUpRight />
       </div>
     </div>
